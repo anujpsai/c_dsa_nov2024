d7ba5ffd1bd016466fbfca53b3e7462619152d62
anujpsaiexit <anujsai2005@gmail.com>
2024-11-05 19:02:50 +0530

    changed

---
+++ assignment-1/problem-1.c
@@ -0,0 +1,12 @@
+//check if a number is positive
+
+#include<stdio.h>
+int main () {
+    int x;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    if (x>0){
+        printf("Entered number is positive.");
+    }
+}
+
