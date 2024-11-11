aec9adf2b3afaa9a0ace4e0d167f72da6693c14e
anujpsaiexit <anujsai2005@gmail.com>
2024-11-11 15:30:16 +0530

    changed

---
+++ day-4 assignment/pb2.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int findFactorial(int num)
+{
+    if (num == 1 || num == 0)
+        return 1;
+    return num * findFactorial(num - 1);
+}
+
+int main(int argCount, char *args[])
+{
+    int inputNumber = atoi(args[1]);
+    int factNumber = findFactorial(inputNumber);
+    printf("Factorial of %d is %d", inputNumber,factNumber);
+}
---
+++ day-5 assignment/pb1.c
@@ -0,0 +1,20 @@
+// find frequency if a number in a array
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argcount, int *args[]){
+    int i=0;
+    int arr[10];
+    int num;
+    printf("enter the number:");
+    for(i=0;i<10;i++){
+        scanf("%d", &num);
+    }
+    for(i=0;i<10;){
+
+    }
+    return 0;
+
+}
